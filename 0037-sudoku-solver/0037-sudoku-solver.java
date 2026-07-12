ec12f82f50de7b57e1993f23e5a7741ae3b7c758
Tanush649345 <tanush.unna649345@gmail.com>
2026-07-13 01:05:32 +0530

    Time: 374 ms (39.69%), Space: 43 MB (34.54%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,21 +1,21 @@
 class Solution {
-
-    public void solveSudoku(char[][] board) {
-        solve(board);
+    boolean isvalid(char c,int row,int col,char[][] board){
+        for(int i = 0;i < 9;i++){
+            if(board[row][i] == c) return false;
+            if(board[i][col] == c) return false;
+            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)] == c) return false;
+        }
+        return true;
     }
-
-    public boolean solve(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == '.') {
-                    for (char c = '1'; c <= '9'; c++) {
-                        if (isValid(board, i, j, c)) {
+    boolean solve(char[][] board){
+        for(int i =0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char c = '1';c<='9';c++){
+                        if(isvalid(c,i,j,board)){
                             board[i][j] = c;
-
-                            if (solve(board))
-                                return true;
-
-                            board[i][j] = '.';
+                            if(solve(board)==true) return true;
+                            else board[i][j]='.';
                         }
                     }
                     return false;
@@ -24,19 +24,8 @@ class Solution {
         }
         return true;
     }
-
-    public boolean isValid(char[][] board, int row, int col, char c) {
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == c)
-                return false;
-
-            if (board[row][i] == c)
-                return false;
-
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
-                return false;
-        }
-
-        return true;
+    
+    public void solveSudoku(char[][] board) {
+        solve(board);
     }
 }
