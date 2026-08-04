4f5d49f20268f46c09f03c7bbc6f6f1c40f8a5af
Tanush649345 <tanush.unna649345@gmail.com>
2026-08-04 23:44:12 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,9 +1,9 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==0 && n==1) return 1;
+        if(n==0 || n==1) return 1;
         int prev = 1;
         int prev2 = 1;
-        int curr;
+        int curr ;
         for(int i = 2;i<=n;i++){
             curr = prev + prev2;
             prev2 = prev;prev=curr;
